a182239334b1a0071a0914097f65c889768ad154
mixstix <mixstixx@gmail.com>
2015-01-28 01:15:57 +0800

    Added toggleClass to switch filter on and off

--- app.js
+++ app.js
@@ -7,16 +7,50 @@ $(document).ready(function() {
         $(this).remove(); 
     });
     $('#filters').on('click', '.onsale-filter', function() {
-        $('.highlighted').removeClass('highlighted'); 
-        $('.col').filter('.onsale').addClass('highlighted');
+    $('.col').filter('.expiring').removeClass('highlighted'); 
+    $('.col').filter('.onsale').toggleClass('highlighted');
     });
     $('#filters').on('click', '.expiring-filter', function() {
-        $('.highlighted').removeClass('highlighted'); 
-        $('.col').filter('.expiring').addClass('highlighted');
+        $('.col').filter('.onsale').removeClass('highlighted'); 
+        $('.col').filter('.expiring').toggleClass('highlighted');
     });
     
-    $('.onsale-filter', '.expiring-filter').on('click', function() {
-        $(this).toggleClass('highlighted');
+//    $('.onsale-filter', '.expiring-filter').on('click', function() {
+//        $(this).toggleClass('highlighted');
+//    });
+});
+
+/*
+window.addEventListener('load', function() {
+    var prices = document.querySelectorAll('.get-price');
+    prices = [].forEach.call( prices, function(e) {
+        e.addEventListener('click', function() {
+            var col = this.parentNode;
+            var p = document.createElement('p');
+            p.className = 'price';
+            p.innerHTML = 'From $' + col.getAttribute('data-price');
+            col.appendChild(p);
+            this.remove();
+        });
     });
 });
+*/
+
+
+//    $('.get-price').on('click', function() {
+//        var col = $(this).closest('.col')
+//        var amount = col.data('price');
+//        var price = $('<p class="price">From $'+amount+'</p>');
+//        col.append(price);
+//        $(this).remove(); 
+//    });
+//    document.getElementById('#filters').on('click', '.onsale-filter', function() {
+//    $('.col').filter('.expiring').removeClass('highlighted'); 
+//    $('.col').filter('.onsale').toggleClass('highlighted');
+//    });
+//    $('#filters').on('click', '.expiring-filter', function() {
+//        $('.col').filter('.onsale').removeClass('highlighted'); 
+//        $('.col').filter('.expiring').toggleClass('highlighted');
+//    });
+
 
